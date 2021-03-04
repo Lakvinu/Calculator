9acabd17a7f76598419734f5483758ab87841cbc
lakvinu <sandeepalhah@gmail.com>
2021-03-04 18:06:20 +1100

    changed entry size

--- Simple-Calculator/v1.py
+++ Simple-Calculator/v1.py
@@ -22,6 +22,7 @@ def button_command(symbol):
 
     elif symbol == 'c':
         clear()
+
     else:
         if symbol in symbols and input_line[-1] in symbols:
             input_line[-1] = symbol
@@ -64,16 +65,16 @@ def rgbtohex(r,g,b):
 equation = StringVar()
 top  = Frame(window, bg= rgbtohex(170,38,54))
 top.pack(side = TOP, expand = True, fill = X)
-input_place = Entry(top, font =("Arial", 20), textvariable=equation)
-input_place.pack(anchor='center', pady=10, ipady=10, ipadx = 40)
+input_place = Entry(top, font =("Arial", 20), textvariable=equation, width=26)
+input_place.pack(anchor='center', pady=10, ipady=10, ipadx = 10)
 
 #input_place.grid(ipadx = 70, ipady = 20,  padx = 10, pady = 10)
 
-bottom = Frame(window, bg= rgbtohex(43,135,209))
+bottom = Frame(window, bg= rgbtohex(25,73,114))
 bottom.pack(side=LEFT, fill=BOTH, expand=True)
 bottom.pack_propagate(False)
 
-key = {0: ['7','8','9', '+'], 1: ['4','5','6', '−'], 2: ['1','2','3', '×'], 3: ['c', '0', '=', '÷']}
+key = {0: ['7','8','9', '+'], 1: ['4','5','6', '−'], 2: ['1','2','3', '×'], 3: ['C', '0', '=', '÷']}
 
 for i in range(4):
 
@@ -82,9 +83,23 @@ for i in range(4):
     for j in range(4):
 
         cur = numb[j]
+        left_c, right_c, top_c, bottom_c = 2,2,2,2
 
-        new_button = Button(bottom, text = cur, font = ("Arial", 20), command =lambda sym = cur: button_command(sym))
-        new_button.grid(row = i, column = j, ipadx = 30, padx = 2, ipady = 10, pady = 2)
+        if i == 0:
+            top_c = 4
+
+        if i == 3:
+            bottom_c = 4
+
+        if j == 0:
+            left_c = 4
+
+        if j == 3:
+            right_c = 4
+
+
+        new_button = Button(bottom, width = 2, text = cur, font = ("Arial", 20), command =lambda sym = cur: button_command(sym))
+        new_button.grid(row = i, column = j, ipadx = 30, padx = (left_c, right_c), ipady = 10, pady = (top_c, right_c))
 
 
 window.mainloop()
